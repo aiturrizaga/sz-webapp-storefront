8cde1c23cd69ab26af2362b09b8fad0954e69079
Alvaro Iturrizaga <aiturrizaga@vallegrande.edu.pe>
2024-01-27 17:22:22 -0500

    Checkout - Delivery methods

--- src/app/modules/checkout/checkout.page.ts
+++ src/app/modules/checkout/checkout.page.ts
@@ -2,16 +2,19 @@ import { Component, effect, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopCartService } from '../../core/services/shop-cart.service';
 import { CartItem } from '../../core/interfaces';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './checkout.page.html',
   styles: []
 })
 export class CheckoutPage implements OnInit {
 
+  public deliveryMethod: string = '';
+  public deliveryAmount: number = 0;
   public subtotal: number = 0;
   public totalAmount: number = 0;
   public cartItems: CartItem[] = this.shopCartService.cart().items;
@@ -24,6 +27,25 @@ export class CheckoutPage implements OnInit {
 
   ngOnInit(): void {
     window.scroll(0, 0);
+    this.setDeliveryMethod('express');
+  }
+
+  setDeliveryMethod(method: string): void {
+    this.deliveryMethod = method;
+
+    switch (method) {
+      case 'express':
+        this.deliveryAmount = 30.00;
+        break;
+      case 'classic':
+        this.deliveryAmount = 12.00;
+        break;
+      default:
+        this.deliveryAmount = 10.00;
+        break;
+    }
+
+    this.totalAmount = this.subtotal + this.deliveryAmount;
   }
 
 }
